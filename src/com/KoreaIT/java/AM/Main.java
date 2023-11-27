55310df1ac3b895fbe8a64ee2ab3b977116ae22f
hyejungHwang <odog0628@gmail.com>
2023-11-27 23:23:43 +0900

    게시물 상세보기 구현, 날짜 아직

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -14,44 +14,62 @@ public class Main {
     while (true) {
       System.out.print("명령어 ) ");
       String cmd = sc.nextLine().trim();
-      if (cmd.length() == 0){
+      if (cmd.length() == 0) {
         continue;
       }
-      if (cmd.equals("system exit")){
+      if (cmd.equals("system exit")) {
         break;
       }
 
-      if(cmd.equals("article write")){
+      if (cmd.equals("article write")) {
         int id = lastArticleId + 1;
         lastArticleId = id;
         System.out.print("제목 : ");
         String title = sc.nextLine();
         System.out.print("내용 : ");
         String body = sc.nextLine();
-//        System.out.printf("%s, %s\n", title, body);
 
         Article article = new Article(id, title, body);
         articles.add(article);
         System.out.printf("%d번 글이 생성되었습니다.\n", id);
 
-      } else if (cmd.equals("article list")){
-        if (articles.size() == 0){
+      } else if (cmd.equals("article list")) {
+        if (articles.size() == 0) {
           System.out.println("게시글이 없습니다.");
         } else {
           System.out.println(" 번호 : 제목");
           for (int i = articles.size() - 1; i >= 0; i--) {
             Article article = articles.get(i);
-            System.out.printf("  %d  :   %s\n", article.id, article.title);
+            System.out.printf("  %d  :  %s\n", article.id, article.title);
           }
         }
-        
+      } else if (cmd.startsWith("article detail ")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]); // 문자를 정수로 변환
+
+        Article foundArticle = null;
+        for(int i = 0; i <articles.size(); i++){
+          Article article = articles.get(i);
+          if(article.id == id){
+            foundArticle = article;
+            break;
+          }
+        }
+        if(foundArticle == null) {
+          System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+        } else {
+          System.out.printf("번호 : %d\n", foundArticle.id);
+          System.out.printf("날짜 : 2023-11-12 12:12:12\n");
+          System.out.printf("제목 : %s\n", foundArticle.title);
+          System.out.printf("내용 : %s\n", foundArticle.body);
+        }
+
       } else {
         System.out.println("존재하지 않는 명령어입니다.");
       }
     }
-
-    sc.close();
-    System.out.println("== 프로그램 종료 ==");
+      sc.close();
+      System.out.println("== 프로그램 종료 ==");
   }
 }
 
